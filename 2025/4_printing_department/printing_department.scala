b466dfd21650d69c90c4401e2bef5fa119e2c0dd
Daniel Visser <dan.visser@elliptic.co>
2026-01-05 19:26:58 +0000

    printing department pt.1

--- 2025/4_printing_department/printing_department.scala
+++ 2025/4_printing_department/printing_department.scala
@@ -1,18 +1,33 @@
-object printingDepartment extends App {
-  main()
+import scala.util.chaining.*
 
-  case class State(code: Int, passesThroughZero: Int)
+object PrintingDepartment extends App {
+  val Roll = '@'
+  val InputPath = "4_printing_department/input.txt"
 
-  def main(): Unit = {
-
-    val states = io.Source
-      .fromFile("4_printing_department/input.txt")
-      .getLines()
-      .flatMap(parseRills)
-      .foreach(println)
+  io.Source.fromFile(InputPath)
+  .getLines()
+  .map(_.toArray)
+  .toArray
+  .pipe { grid =>
+    grid.indices.flatMap(i =>
+      grid(i).indices.collect {
+        case j if grid(i)(j) == Roll =>
+          val adj = neighbors(i, j, grid).count(_ == Roll)
+          if (adj < 4) 1 else 0
+      }
+    ).sum
   }
+  .pipe(println)
 
-  def parseRills(line: String): List[String] = {
-    line.split("\\s+").toList
-  }
+  def neighbors(row: Int, col: Int, grid: Array[Array[Char]]): Seq[Char] =
+    for {
+      dr <- -1 to 1
+      dc <- -1 to 1
+      if dr != 0 || dc != 0
+      r = row + dr
+      c = col + dc
+      if r >= 0 && r < grid.length
+      if c >= 0 && c < grid(r).length
+    } yield grid(r)(c)
+
 }
