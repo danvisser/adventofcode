4fc31cc778e6232c60b8068e259811bd1373a946
Daniel Visser <dan.visser@elliptic.co>
2026-01-05 22:20:26 +0000

    day 4 - pt. 2

--- 2025/4_printing_department/printing_department.scala
+++ 2025/4_printing_department/printing_department.scala
@@ -1,33 +1,58 @@
-import scala.util.chaining.*
-
 object PrintingDepartment extends App {
-  val Roll = '@'
-  val InputPath = "4_printing_department/input.txt"
+  private val Roll = '@'
+  private val Empty = '.'
 
-  io.Source.fromFile(InputPath)
-  .getLines()
-  .map(_.toArray)
-  .toArray
-  .pipe { grid =>
-    grid.indices.flatMap(i =>
-      grid(i).indices.collect {
-        case j if grid(i)(j) == Roll =>
-          val adj = neighbors(i, j, grid).count(_ == Roll)
-          if (adj < 4) 1 else 0
-      }
-    ).sum
-  }
-  .pipe(println)
+  val initialGrid = io.Source
+    .fromFile("4_printing_department/input.txt")
+    .getLines()
+    .map(_.toArray)
+    .toArray
 
-  def neighbors(row: Int, col: Int, grid: Array[Array[Char]]): Seq[Char] =
-    for {
+  val part1 = removeRolls(minNeighbors = 4, initialGrid)._2
+  val part2 = keepRemovingRolls(minNeighbors = 4, initialGrid)
+
+  println(s"Part 1: $part1")
+  println(s"Part 2: $part2")
+
+  private def countRollNeighbors(
+      grid: Array[Array[Char]],
+      row: Int,
+      col: Int
+  ): Int = {
+    val neighbors = for {
       dr <- -1 to 1
       dc <- -1 to 1
       if dr != 0 || dc != 0
       r = row + dr
       c = col + dc
       if r >= 0 && r < grid.length
-      if c >= 0 && c < grid(r).length
     } yield grid(r)(c)
+    neighbors.count(_ == Roll)
+  }
 
+  private def removeRolls(
+      minNeighbors: Int,
+      grid: Array[Array[Char]]
+  ): (Array[Array[Char]], Int) = {
+    var count = 0
+    val newGrid = grid.zipWithIndex.map { case (row, r) =>
+      row.zipWithIndex.map { case (cell, c) =>
+        if (cell == Roll && countRollNeighbors(grid, r, c) < minNeighbors) {
+          count += 1
+          Empty
+        } else cell
+      }.toArray
+    }.toArray
+    (newGrid, count)
+  }
+
+  private def keepRemovingRolls(
+      minNeighbors: Int,
+      grid: Array[Array[Char]],
+      removed: Int = 0
+  ): Int = {
+    val (newGrid, count) = removeRolls(minNeighbors, grid)
+    if (count == 0) removed
+    else keepRemovingRolls(minNeighbors, newGrid, removed + count)
+  }
 }
